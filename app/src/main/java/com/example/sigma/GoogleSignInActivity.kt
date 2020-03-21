adb3c4a0864f455d67073fe50c78d43c3b65f0fd
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2020-03-21 13:24:50 -0300

    Inicio de Sesion con Google

---
+++ app/src/main/java/com/example/sigma/GoogleSignInActivity.kt
@@ -0,0 +1,108 @@
+package com.example.sigma
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_google_sign_in.*
+
+class GoogleSignInActivity : BaseActivity(), View.OnClickListener {
+
+    //[Variables]
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_google_sign_in)
+
+        setProgressBar(R.id.progress_bar)
+
+        btn_sign_in_google.setOnClickListener(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            goToMainActivity()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            }catch (e: ApiException){
+                Log.w(TAG, "Inicio de Sesion en Google Fallo", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        showProgressBar()
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:exitoso")
+                    goToMainActivity()
+                }else{
+                    Log.w(TAG, "signInWithCredential:fallido", task.exception)
+                    Snackbar.make(main_layout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                }
+                hideProgressBar()
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun goToMainActivity(){
+        val intentMainActivity = Intent(this, MainActivity::class.java)
+        startActivity(intentMainActivity)
+        finish()
+    }
+
+    override fun onClick(v : View) {
+        when(v.id){
+            R.id.btn_sign_in_google -> signIn()
+        }
+    }
+
+    companion object {
+        private const val TAG = "GoogleSingInActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
